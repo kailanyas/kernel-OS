7cbffc3ce4c89263c605cae76d59e49e85372cdc
ThiagoTonelli <thiagktonelli@gmail.com>
2024-09-27 09:41:36 -0300

    incluindo irq.c no montador (#16)(#2).

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,6 +25,11 @@ nasm -f elf32 -o start.o start.asm
              -finline-functions -nostdinc -fno-builtin -fno-pie 
              -I./include -c -m32                                
              -o isrs.o isrs.c
+             
+             gcc -Wall -O -fstrength-reduce -fomit-frame-pointer             \
+             -finline-functions -nostdinc -fno-builtin -fno-pie \
+             -I./include -c -m32                                \
+             -o irq.o irq.c
 
-ld -m elf_i386 -T link.ld -o kernel.bin start.o 
+ld -m elf_i386 -T link.ld -o kernel.bin start.o main.o scrn.o gdt.o idt.o isrs.o irq.o
 echo Feito!
