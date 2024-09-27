f02efb0ab138ae4e6fff0f45f4a1f62df06357bf
kailany <kailanyalves04@gmail.com>
2024-09-26 21:29:46 -0300

    incluindo declarações das funções de idt.c no arquivo de cabeçalho (#14)(#11).

--- include/system.h
+++ include/system.h
@@ -20,4 +20,8 @@ extern void init_video();
 extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
 void gdt_install();
 
+// idt.c
+extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+extern void idt_install();
+
 #endif
