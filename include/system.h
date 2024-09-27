aaf570eb574d3f4cbda98e018e234d1d8b6f8ae3
ThiagoTonelli <thiagktonelli@gmail.com>
2024-09-27 09:21:14 -0300

    adicionando declarações de funções no arquivo de cabeçalho (5)(#11).

--- include/system.h
+++ include/system.h
@@ -24,4 +24,47 @@ void gdt_install();
 extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
 extern void idt_install();
 
+// isrs.c
+struct regs
+{
+    unsigned int ds, es, fs, gs;                          /* últimos segmentos puxados */
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* puxado por 'pusha' */
+    unsigned int int_no, err_code;                        /* nosso 'push byte #' e ecodes (códigos de erro) faz isto */
+    unsigned int eip, cs, eflags, useresp, ss;            /* puxados pelo processador automaticamente */
+};
+extern void isrs_install();
+extern void fault_handler(struct regs *r);
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
 #endif
